ca6ec95aa0b8810a6f4092eb1f5feab128cda3d8
DESKTOP-FFINH2R\herdito <herditoibnu@gmail.com>
2017-05-30 10:16:48 +0700

    edit penghuni

--- code_v3/production/kamar_list4.php
+++ code_v3/production/kamar_list4.php
@@ -56,6 +56,7 @@
                   <th>Tanggal Lahir</th>
                   <th>Pembayaran Terakhir</th>
                   <th>Pembayaran Jatuh Tempo</th>
+                  <th>Edit</th>
           <!--         <th>Edit</th>
                   <th>Hapus</th> -->
                 </tr>
@@ -74,6 +75,7 @@
                   <td>12/25/1993</td>
                   <td style="background-color: green; color: white;">Sudah Bayar</td>
                   <td>06/14/2017</td>
+                  <td> <a href="kamar_edit4.php" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a></td>
                 </tr>
                 <?php } ?>
                 <?php for($i=0;$i<5;$i++){ ?>
@@ -81,6 +83,7 @@
                   <td><?php echo $i+6;?></td>
                   <td>AC</td>
                   <td>Kosong</td>
+                  <td>-</td>
                   <td>-</td>
                   <td>-</td>
                   <td>-</td>
@@ -100,6 +103,7 @@
                   <td>10/19/1994</td>
                   <td style="background-color: red; color: white;">Belum Bayar</td>
                   <td>05/20/2017</td>
+                  <td> <a href="kamar_edit4.php" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a></td>
                 </tr>
                 <?php } ?>
                 <?php for($i=0;$i<5;$i++){ ?>
@@ -107,6 +111,7 @@
                   <td><?php echo $i+16;?></td>
                   <td>AC + Kamar Mandi Dalam</td>
                   <td>Kosong</td>
+                  <td>-</td>
                   <td>-</td>
                   <td>-</td>
                   <td>-</td>
